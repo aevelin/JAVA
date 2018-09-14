3a791b144626368d8bd67fdec350df20f8c516e8
Evelin Arust <evelin.arust@khk.ee>
2018-09-14 20:13:49 +0300

    Calculations

--- src/ee/esimene/Hello.java
+++ src/ee/esimene/Hello.java
@@ -5,7 +5,17 @@ public class Hello {
 
         System.out.println("Hello, Evelin!");
 
-        int myFirstNumber = 11;
+        int myFirstNumber = 11 + 11;
         System.out.println(myFirstNumber);
+
+        int mySecondNumber = (11 + 11) + (11 * 11);
+        int myThirdNumber = 6;
+        int myFourthNumber = myFirstNumber + 7;
+
+        int myTotal = mySecondNumber + myThirdNumber + myFourthNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
     }
 }
