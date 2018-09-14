90933dd0cc3297bd7c687b06cfa4e6a10578b39f
Evelin Arust <evelin.arust@khk.ee>
2018-09-14 20:02:21 +0300

    Variable myFirstNumber

--- src/ee/esimene/Hello.java
+++ src/ee/esimene/Hello.java
@@ -2,6 +2,10 @@ package ee.esimene;
 
 public class Hello {
     public static void main(String[] args) {
+
         System.out.println("Hello, Evelin!");
+
+        int myFirstNumber = 5;
+        System.out.println(myFirstNumber);
     }
 }
